9d5570c440848f98933f8e18adf251ee4e69278b
shanghuaw <253950017@qq.com>
2024-07-26 21:07:40 +0800

    Modify README

--- Code_workspace/cooperation_GE/src/120T.bat
+++ Code_workspace/cooperation_GE/src/120T.bat
@@ -1,30 +1,30 @@
 @echo off
-start "GEABT-4" cmd /k python execute.py 4 120
+start "ABTE-4" cmd /k python execute.py 4 120
 timeout /t 2
-start "GEABT-8" cmd /k python execute.py 8 120
+start "ABTE-8" cmd /k python execute.py 8 120
 timeout /t 2
-start "GEABT-12" cmd /k python execute.py 12 120
+start "ABTE-12" cmd /k python execute.py 12 120
 timeout /t 2
-start "GEABT-16" cmd /k python execute.py 16 120
+start "ABTE-16" cmd /k python execute.py 16 120
 timeout /t 2
-start "GEABT-20" cmd /k python execute.py 20 120
+start "ABTE-20" cmd /k python execute.py 20 120
 timeout /t 2
-start "GEABT-24" cmd /k python execute.py 24 120
+start "ABTE-24" cmd /k python execute.py 24 120
 timeout /t 2
-start "GEABT-28" cmd /k python execute.py 28 120
+start "ABTE-28" cmd /k python execute.py 28 120
 timeout /t 2
-start "GEABT-32" cmd /k python execute.py 32 120
+start "ABTE-32" cmd /k python execute.py 32 120
 timeout /t 2
-start "GEABT-36" cmd /k python execute.py 36 120
+start "ABTE-36" cmd /k python execute.py 36 120
 timeout /t 2
-start "GEABT-40" cmd /k python execute.py 40 120
+start "ABTE-40" cmd /k python execute.py 40 120
 timeout /t 2
-start "GEABT-44" cmd /k python execute.py 44 120
+start "ABTE-44" cmd /k python execute.py 44 120
 timeout /t 2
-start "GEABT-48" cmd /k python execute.py 48 120
+start "ABTE-48" cmd /k python execute.py 48 120
 timeout /t 2
-start "GEABT-52" cmd /k python execute.py 52 120
+start "ABTE-52" cmd /k python execute.py 52 120
 timeout /t 2
-start "GEABT-56" cmd /k python execute.py 56 120
+start "ABTE-56" cmd /k python execute.py 56 120
 timeout /t 2
-start "GEABT-60" cmd /k python execute.py 60 120
+start "ABTE-60" cmd /k python execute.py 60 120
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # shanghua.github.io
  
 Auction-Based Behavior Tree Evolution for Heterogeneous Multi-Agent Systems.
+
+1. The 'cooperation_GE' folder contains the implementation of the ABTE method. Running src/execute.py will execute the program based on the input number of robots and tasks. The 'cooperation_move' folder contains the baseline method, which is used for comparison with ABTE, and its execution file is demo/src/execute.py.
+
+2. The environmental data set in the paper refers to demo/mission.py, and the robot data set in the paper refers to demo/mission_behaviors.py.
+
+3. The raw data used in the paper refers to cooperation_GE/data, demo/data.
